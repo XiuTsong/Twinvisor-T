9310501e0e1d959c799c65fa41e05e6c1ac2261d
wyc <1042718192@qq.com>
2024-11-08 19:28:24 +0800

    port: Add sched.c/h

---
+++ linux/include/s-visor/sched/sched.h
@@ -0,0 +1,12 @@
+/*
+ * @Description: titanium sched.h
+ * @Date: 2024-11-08 16:29:21
+ */
+
+#ifndef __SVISOR_SCHED_H__
+#define __SVISOR_SCHED_H__
+
+void tianium_vm_sched_init(void);
+void tianium_vm_sched(void);
+
+#endif
