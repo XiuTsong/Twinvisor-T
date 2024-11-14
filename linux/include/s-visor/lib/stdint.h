487024edb3c0c9772dc36fb0c6a69ce3bae80bd1
wyc <1042718192@qq.com>
2024-11-14 10:28:48 +0800

    port: Add necessary lib header files

---
+++ linux/include/s-visor/lib/stdint.h
@@ -0,0 +1,18 @@
+/*
+ * @Description: tianium stdint.h
+ * @Date: 2024-11-11 19:21:32
+ */
+
+#ifndef __SVISOR_STDINT_H__
+#define __SVISOR_STDINT_H__
+
+#include <linux/types.h>
+
+#ifndef __ASSEMBLER__
+
+typedef long register_t;
+typedef unsigned long u_register_t;
+
+#endif
+
+#endif
