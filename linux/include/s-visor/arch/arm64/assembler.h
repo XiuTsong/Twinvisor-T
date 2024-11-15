2aa36252266567764f5a6729a752ec9a0bd430e1
wyc <1042718192@qq.com>
2024-11-15 15:21:54 +0800

    port: Add adr_l/ldr_l/str_l macro for feature use

---
+++ linux/include/s-visor/arch/arm64/assembler.h
@@ -0,0 +1,53 @@
+/*
+ * @Date: 2024-11-15 09:55:36
+ */
+
+#ifndef __SVISOR_ASSEMBLER_H__
+#define __SVISOR_ASSEMBLER_H__
+
+/* Copy from <asm/assembler.h> */
+
+#ifdef __ASSEMBLER__
+/*
+ * Pseudo-ops for PC-relative adr/ldr/str <reg>, <symbol> where
+ * <symbol> is within the range +/- 4 GB of the PC.
+ */
+	/*
+	 * @dst: destination register (64 bit wide)
+	 * @sym: name of the symbol
+	 */
+	.macro	_adr_l, dst, sym
+	adrp	\dst, \sym
+	add	\dst, \dst, :lo12:\sym
+	.endm
+
+	/*
+	 * @dst: destination register (32 or 64 bit wide)
+	 * @sym: name of the symbol
+	 * @tmp: optional 64-bit scratch register to be used if <dst> is a
+	 *       32-bit wide register, in which case it cannot be used to hold
+	 *       the address
+	 */
+	.macro	_ldr_l, dst, sym, tmp=
+	.ifb	\tmp
+	adrp	\dst, \sym
+	ldr	\dst, [\dst, :lo12:\sym]
+	.else
+	adrp	\tmp, \sym
+	ldr	\dst, [\tmp, :lo12:\sym]
+	.endif
+	.endm
+
+	/*
+	 * @src: source register (32 or 64 bit wide)
+	 * @sym: name of the symbol
+	 * @tmp: mandatory 64-bit scratch register to calculate the address
+	 *       while <src> needs to be preserved.
+	 */
+	.macro	_str_l, src, sym, tmp
+	adrp	\tmp, \sym
+	str	\src, [\tmp, :lo12:\sym]
+	.endm
+#endif
+
+#endif
