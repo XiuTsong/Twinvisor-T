b083375acf9d9d8ba37b1d7d802dfe2ca5136fad
wyc <1042718192@qq.com>
2024-11-25 14:31:21 +0800

    port: Add printf related files in s-visor

---
+++ linux/include/s-visor/lib/stdio.h
@@ -0,0 +1,23 @@
+/*
+ * @Date: 2024-11-25 10:13:34
+ */
+
+#ifndef __SVISOR_STDIO_H__
+#define __SVISOR_STDIO_H__
+
+#define EOF            -1
+
+#define __printflike(fmtarg, firstvararg) \
+		__attribute__((__format__ (__printf__, fmtarg, firstvararg)))
+
+int printf(const char *fmt, ...) __printflike(1, 2);
+int snprintf(char *s, unsigned long n, const char *fmt, ...) __printflike(3, 4);
+
+#ifdef STDARG_H
+int vprintf(const char *fmt, va_list args);
+#endif
+
+int putchar(int c);
+int puts(const char *s);
+
+#endif
