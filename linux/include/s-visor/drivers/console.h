92a88a5c2f7412b82fc8e73432ed2d24a97779b4
wyc <1042718192@qq.com>
2024-11-25 14:30:30 +0800

    port: Add pl011 console related functions in s-visor

--- linux/include/s-visor/drivers/console.h
+++ linux/include/s-visor/drivers/console.h
@@ -36,4 +36,57 @@
 #define PLAT_ARM_BL31_RUN_UART_CLK_IN_HZ	1
 #define ARM_CONSOLE_BAUDRATE		115200
 
+#ifndef __ASSEMBLY__
+
+#include <s-visor/lib/stdint.h>
+
+typedef struct _console {
+	struct _console *next;
+	/*
+	 * Only the low 32 bits are used. The type is u_register_t to align the
+	 * fields of the struct to 64 bits in AArch64 and 32 bits in AArch32
+	 */
+	u_register_t flags;
+	int (*const putc)(int character, struct _console *console);
+	int (*const getc)(struct _console *console);
+	int (*const flush)(struct _console *console);
+	/* Additional private driver data may follow here. */
+} _console_t;
+
+/* offset macro assertions for console_t */
+#include <s-visor/drivers/console_assertions.h>
+
+/*
+ * NOTE: There is no publicly accessible console_register() function. Consoles
+ * are registered by directly calling the register function of a specific
+ * implementation, e.g. console_16550_register() from <uart_16550.h>. Consoles
+ * registered that way can be unregistered/reconfigured with below functions.
+ */
+/* Remove a single console_t instance from the console list. Return a pointer to
+ * the console that was removed if it was found, or NULL if not. */
+_console_t *_console_unregister(_console_t *console);
+/* Returns 1 if this console is already registered, 0 if not */
+int _console_is_registered(_console_t *console);
+/*
+ * Set scope mask of a console that determines in what states it is active.
+ * By default they are registered with (CONSOLE_FLAG_BOOT|CONSOLE_FLAG_CRASH).
+ */
+void _console_set_scope(_console_t *console, unsigned int scope);
+
+/* Switch to a new global console state (CONSOLE_FLAG_BOOT/RUNTIME/CRASH). */
+void _console_switch_state(unsigned int new_state);
+/* Output a character on all consoles registered for the current state. */
+int _console_putc(int c);
+/* Read a character (blocking) from any console registered for current state. */
+int _console_getc(void);
+/* Flush all consoles registered for the current state. */
+int _console_flush(void);
+
+/* REMOVED on AArch64 -- use console_<driver>_register() instead! */
+int _console_init(uintptr_t base_addr,
+		 unsigned int uart_clk, unsigned int baud_rate);
+void _console_uninit(void);
+
+#endif /* __ASSEMBLY__ */
+
 #endif /* SVISOR_CONSOLE_H */
