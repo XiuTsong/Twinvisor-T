aae6c32c87a1cb123a2418b62f18687b1c6ae293
wyc <1042718192@qq.com>
2024-11-25 19:30:42 +0800

    port: Complete kvm_relay module

--- linux/include/s-visor/virt/kvm_relay.h
+++ linux/include/s-visor/virt/kvm_relay.h
@@ -7,7 +7,9 @@
 #define __S_VISOR_KVM_RELAY_H__
 
 #include <s-visor/virt/kvm_host_common.h>
+#include <s-visor/virt/vm.h>
 
 kvm_smc_req_t *get_smc_req_region_sec(unsigned int core_id);
+int decode_kvm_vm_exit(struct titanium_state *state, uint32_t core_id, uint32_t vcpu_id);
 
 #endif
